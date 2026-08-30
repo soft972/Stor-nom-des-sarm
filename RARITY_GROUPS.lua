1ce3931c776df2c6bf227f34701bc1e299c76a23
soft972 <KAMIKAZ_soft@hotmail.com>
2026-08-30 15:16:06 -0300

    Refactor item names in RARITY_GROUPS.lua
    Updated item names in RARITY_GROUPS for clarity and consistency.

--- RARITY_GROUPS.lua
+++ RARITY_GROUPS.lua
@@ -1,30 +1,31 @@
 local RARITY_GROUPS = {
-    [Color3.fromRGB(150, 150, 150)] = {
-        "fists", "watering", "perch", "bass", "salmon", "bandage", "regular", "prise"
-    },
-    [Color3.fromRGB(0, 89, 255)] = {
-        "storarm3", "p226", "découpeur", "poéle à frire", "axe", "regular pot", "corn seeds",
-        "regular fertilizer", "regular soil", "coelacanth", "ultimate fishing rod",
-        "advanced fishing rod", "sawnoff", "uzi", "hunting rifle", "brainrot slapper",
-        "emergency care kit", "clè", "lame de commutation", "balai en diamant", "glock",
-        "pre workout", "metal baseball bat", "wrench"
-    },
-    [Color3.fromRGB(0, 255, 0)] = {
-        "g3", "sunflower seeds", "northern pike", "dolphin fish", "pro fishing rod", "c9",
-        "marteau", "batte de baseball", "énergie taureau", "sac de sang"
-    },
-    [Color3.fromRGB(170, 0, 255)] = {
-        "draco", "tomato seeds", "premium soil", "organic fertilizer", "tuna", "combat axe",
-        "barbed baseball bat", "m24", "firework launcher", "double tonneau", "energy shot",
-        "machete", "skorpion", "molotov", "grenade", "sledge hammer", "shovel"
-    },
-    [Color3.fromRGB(255, 193, 5)] = {
-        "m16", "golden pot", "golden tomato seeds", "diamond pot", "marlin", "sailfish",
-        "rpg", "ak47", "mp5", "remington", "crossbow", "tactical axe", "tactical shovel",
-        "tactical knife"
-    },
-    [Color3.fromRGB(255, 0, 4)] = {
-        "anaconda", "m249"
-    }
+	[Color3.fromRGB(150, 150, 150)] = {
+		"fists", "watering can", "perch", "bass", "salmon", "bandage", "regular fishing rod", "prise de verrou",
+		"glass"
+	},
+	[Color3.fromRGB(0, 255, 247)] = {
+		"storarm3", "p226", "découpeur", "poéle à frire", "axe", "regular pot", "corn seeds",
+		"regular fertilizer", "regular soil", "coelacanth", "ultimate fishing rod",
+		"advanced fishing rod", "sawnoff", "uzi", "hunting rifle", "brainrot slapper",
+		"emergency care kit", "clè", "lame de commutation", "balai en diamant", "glock",
+		"pre workout", "metal baseball bat", "wrench"
+	},
+	[Color3.fromRGB(0, 255, 0)] = {
+		"g3", "sunflower seeds", "northern pike", "dolphin fish", "pro fishing rod", "c9",
+		"marteau", "baseball bat", "énergie taureau", "sac de sang"
+	},
+	[Color3.fromRGB(179, 0, 255)] = {
+		"draco", "tomato seeds", "premium soil", "organic fertilizer", "tuna", "combat axe",
+		"barbed baseball bat", "m24", "firework launcher", "double tonneau", "energy shot",
+		"machete", "skorpion", "molotov", "grenade", "sledge hammer", "shovel"
+	},
+	[Color3.fromRGB(255, 193, 5)] = {
+		"m16", "golden pot", "golden tomato seeds", "diamond pot", "marlin", "sailfish",
+		"rpg", "ak47", "mp5", "remington", "crossbow", "tactical axe", "tactical shovel",
+		"tactical knife"
+	},
+	[Color3.fromRGB(255, 0, 4)] = {
+		"anaconda", "m249"
+	}
 }
 return RARITY_GROUPS
